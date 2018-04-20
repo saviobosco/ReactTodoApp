5bd8e9d4b916c12cd45dc51b8fd48e33a19f636e
Saviobosco <Johnboscoomebe@yahoo.com>
2018-04-20 22:24:07 +0100

    initialized project

---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// App css
+require('style!css!sass!applicationStyles');
+$(document).foundation();
+ReactDOM.render(
+  <p> BoilerPlate3 Project </p>,
+  document.getElementById('app')
+);
